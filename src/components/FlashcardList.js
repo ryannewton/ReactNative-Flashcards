78990d863fac694ad5f23deb78356553d12f53a9
Ryan Newton <newton1988@gmail.com>
2017-01-25 14:46:05 -0500

    React boilerplate for FlashcardList component

---
+++ src/components/FlashcardList.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Import libraries
+import React, { Component } from 'react';
+
+class FlashcardList extends Component {
+	render() {
+		return;
+	}
+}
+
+export default FlashcardList;
